635a873bcae6f11e151c6770fb1df3b1340300da
joshfletcher <joshua5000@outlook.com>
2014-07-08 21:00:06 -0400

    hail stone numbers
    this file i created for some follow up at my math camp, and thought i would show it to you. :-)

---
+++ hail_stone_numbers.py
@@ -0,0 +1,137 @@
+print 'Hail stone numbers!'
+length = 0
+for n in range(1,1000000):
+    print "Number:", n
+    while n != 1:
+        print n,
+        if n % 2 == 0:
+           n = n / 2
+           length = length + 1
+        else:
+            n = (n * 3) + 1
+            length = length + 1
+        if n == 1:
+            print n
+            length = length + 1
+    print "The sequence above contains", length, "numbers"
+    length = 0
+for n in range(1000000,5000000):
+    print "Number:", n
+    while n != 1:
+        print n,
+        if n % 2 == 0:
+           n = n / 2
+           length = length + 1
+        else:
+            n = (n * 3) + 1
+            length = length + 1
+        if n == 1:
+            print n
+            length = length + 1
+    print "The sequence above contains", length, "numbers"
+    length = 0
+for n in range(5000000,10000000):
+    print "Number:", n
+    while n != 1:
+        print n,
+        if n % 2 == 0:
+           n = n / 2
+           length = length + 1
+        else:
+            n = (n * 3) + 1
+            length = length + 1
+        if n == 1:
+            print n
+            length = length + 1
+    print "The sequence above contains", length, "numbers"
+    length = 0
+for n in range(10000000,30000000):
+    print "Number:", n
+    while n != 1:
+        print n,
+        if n % 2 == 0:
+           n = n / 2
+           length = length + 1
+        else:
+            n = (n * 3) + 1
+            length = length + 1
+        if n == 1:
+            print n
+            length = length + 1
+    print "The sequence above contains", length, "numbers"
+    length = 0
+for n in range(30000000,60000000):
+    print "Number:", n
+    while n != 1:
+        print n,
+        if n % 2 == 0:
+           n = n / 2
+           length = length + 1
+        else:
+            n = (n * 3) + 1
+            length = length + 1
+        if n == 1:
+            print n
+            length = length + 1
+    print "The sequence above contains", length, "numbers"
+    length = 0
+for n in range(60000000,90000000):
+    print "Number:", n
+    while n != 1:
+        print n,
+        if n % 2 == 0:
+           n = n / 2
+           length = length + 1
+        else:
+            n = (n * 3) + 1
+            length = length + 1
+        if n == 1:
+            print n
+            length = length + 1
+    print "The sequence above contains", length, "numbers"
+    length = 0
+for n in range(90000000,100000000):
+    print "Number:", n
+    while n != 1:
+        print n,
+        if n % 2 == 0:
+           n = n / 2
+           length = length + 1
+        else:
+            n = (n * 3) + 1
+            length = length + 1
+        if n == 1:
+            print n
+            length = length + 1
+    print "The sequence above contains", length, "numbers"
+    length = 0
+for n in range(100000000,200000000):
+    print "Number:", n
+    while n != 1:
+        print n,
+        if n % 2 == 0:
+           n = n / 2
+           length = length + 1
+        else:
+            n = (n * 3) + 1
+            length = length + 1
+        if n == 1:
+            print n
+            length = length + 1
+    print "The sequence above contains", length, "numbers"
+    length = 0
+for n in range(200000000,400000000):
+    print "Number:", n
+    while n != 1:
+        print n,
+        if n % 2 == 0:
+           n = n / 2
+           length = length + 1
+        else:
+            n = (n * 3) + 1
+            length = length + 1
+        if n == 1:
+            print n
+            length = length + 1
+    print "The sequence above contains", length, "numbers"
+    length = 0
